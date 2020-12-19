3f816c1f38e7cc85ec927093a88ccc64e6b6ad1a
HannaMeyer <hanna.meyer@uni-muenster.de>
2020-12-19 18:00:37 +0100

    some updates

--- src/CaseStudyScenarios.R
+++ src/CaseStudyScenarios.R
@@ -1,32 +1,52 @@
 rm(list=ls())
 setwd("/scratch/tmp/hmeyer1/MappingAOA/")
 
-#install.packages("/home/h/hmeyer1/R/CAST_0.4.1.tar.gz", repos = NULL,
+install.packages("/home/h/hmeyer1/R/CAST_0.4.3.tar.gz", repos = NULL,
+                 lib="/home/h/hmeyer1/R/")
+#install.packages("/home/h/hmeyer1/R/scam_1.2-9.tar.gz", repos = NULL,
 #                 lib="/home/h/hmeyer1/R/")
+
+
 
 library(parallel)
 library(hydroGOF,lib.loc="/home/h/hmeyer1/R/")
 library(virtualspecies,lib.loc="/home/h/hmeyer1/R/")
 library(caret,lib.loc="/home/h/hmeyer1/R/")
 library(CAST,lib.loc="/home/h/hmeyer1/R/")
+library(doParallel)
+library(rgeos,lib.loc="/home/h/hmeyer1/R/")
+#library(rgeos)
+library(scam,lib.loc="/home/h/hmeyer1/R/")
+library(raster)
+ncores=1
 
 
-design <- c("biasedWithOutlier","clustered","biased","random")#, biasedWithOutlier
-countries <- c("Germany","Ireland","France", "Sweden") #if design==biased
-countriesOutlier <- "Turkmenistan" #if design==biasedWithOutlier A single point is set here
-nlusters <- 50 #number of clusters if design==clustered
-maxdist <- 0.6 #maxdist for clustered samples if 
+
+countries <- c("Germany","Ireland","France", "Sweden","Netherlands", "Norway", "United Kingdom","Switzerland",
+               "Austria","Finland","Belgium")
+
+
+countriesOutlier <- c("Turkmenistan","Azerbaijan","Georgia","Syria","Lebanon") #if design==biasedWithOutlier A single point is set here
+#nclusters <- 10 #number of clusters if design==clustered
+maxdist <- 0.6 #maxdist for clustered samples if
+
+design <- c("random")#, biasedWithOutlier
 
 npoints <- c(25,50,75,100) # number of training samples
-#seed <- c(10,20,30)
-seed <- c(10,20)
+#npoints <- c(500)
+seed <- c(10,20,30)
+#seed <- c(10)
 meansPCA <- as.list(as.data.frame(t(expand.grid(c(1,2,3),c(-1,0,1)))))
 sdPCA <-  as.list(as.data.frame(t(expand.grid(c(1,2,3),c(1,2,3)))))
+#meansPCA <- as.list(as.data.frame(t(expand.grid(c(3),c(1)))))
+#sdPCA <-  as.list(as.data.frame(t(expand.grid(c(3),c(1)))))
+
+
 
 simulateResponse <- c("bio2","bio5","bio10", "bio13","bio14","bio19") # variables used to simulate the response
 studyarea <- c(-15, 65, 30, 75) # extent of study area. Default: Europe
 
-predictors_global <- getData('worldclim', var='bio', res=10)
+predictors_global <- raster::getData('worldclim', var='bio', res=10)
 wp <- extent(studyarea)
 predictors <- crop(predictors_global,wp)
 
@@ -46,13 +66,31 @@ for (setting in 1:nrow(settings)){
   npoints <- resultsTable$npoints[setting]
   sdPCA <- resultsTable$sdPCA[[setting]]
   meansPCA <- resultsTable$meansPCA[[setting]]
+  design <- resultsTable$design[[setting]]
 
+  if (design=="clustered"){
+  if (npoints==25){
+    nclusters <- 5 #number of clusters if design==clustered
+  }
+  if (npoints==50){
+    nclusters <- 10 #number of clusters if design==clustered
+  }
+    if (npoints==75){
+      nclusters <- 10 #number of clusters if design==clustered
+    }
+  if (npoints>75){
+    nclusters <- round(npoints/10,0)
+  }
+  resultsTable$nclusters[setting] <- nclusters
+  }else{
+    resultsTable$nclusters[setting] <- NA
+  }
   ## Generate Predictors and Response
   response <- generateSpFromPCA(predictors[[simulateResponse]],
                                 means = meansPCA,
                                 sds = sdPCA,
                                 plot=F)$suitab.raster
-  
+
   ### for clustered sampling
   csample <- function(x,n,nclusters,maxdist,seed){
     set.seed(seed)
@@ -61,11 +99,11 @@ for (setting in 1:nrow(settings)){
     result$clstrID <- 1:length(cpoints)
     for (i in 1:length(cpoints)){
       ext <- rgeos::gBuffer(cpoints[i,], width = maxdist)
-      newsamples <- sp::spsample(ext, n = (n-nclusters)/nclusters, 
+      newsamples <- sp::spsample(ext, n = (n-nclusters)/nclusters,
                                  type="random")
       newsamples$clstrID <- rep(i,length(newsamples))
       result <- rbind(result,newsamples)
-      
+
     }
     result$ID <- 1:nrow(result)
     return(result)
@@ -73,21 +111,24 @@ for (setting in 1:nrow(settings)){
   ###
   set.seed(seed)
   if (design=="clustered"){
-    samplepoints <- csample(mask,npoints,nlusters,maxdist=maxdist,seed=seed)
-  } 
+    samplepoints <- csample(mask,npoints,nclusters,maxdist=maxdist,seed=seed)
+  }
   if (design=="biased"){
-    countryboundaries <- getData("countries")
-    countryboundaries <- countryboundaries[countryboundaries$NAME_ENGLISH%in%c(countries),]
+    countryboundaries <- readRDS("countries_gadm36_sp.rds")
+    countriesselected <- sample(countries,4)
+    countryboundaries <- countryboundaries[countryboundaries$NAME_ENGLISH%in%c(countriesselected),]
     samplepoints <- spsample(countryboundaries,npoints,"random")
   }
   if (design=="biasedWithOutlier"){
-    countryboundaries <- getData("countries")
-    countryboundariesOut <- countryboundaries[countryboundaries$NAME_ENGLISH%in%c(countriesOutlier),]
-    countryboundaries <- countryboundaries[countryboundaries$NAME_ENGLISH%in%c(countries),]
+    countryboundaries <- readRDS("countries_gadm36_sp.rds")
+    countriesselected <- sample(countries,4)
+    countriesOutlier_selected <- sample(countriesOutlier,1)
+    countryboundariesOut <- countryboundaries[countryboundaries$NAME_ENGLISH%in%c(countriesOutlier_selected),]
+    countryboundaries <- countryboundaries[countryboundaries$NAME_ENGLISH%in%c(countriesselected),]
     samplepoints <- spsample(countryboundaries,npoints,"random")
     samplepoints <- rbind(samplepoints,spsample(countryboundariesOut,1,"random"))
-  }  
-  
+  }
+
   if (design=="random"){
     samplepoints <- spsample(mask,npoints,"random")
   }
@@ -95,16 +136,16 @@ for (setting in 1:nrow(settings)){
   # Model training and prediction
 
   trainDat <- extract(predictors,samplepoints,df=TRUE)
-  
+
   if (design=="clustered"){
     trainDat <- merge(trainDat,samplepoints,by.x="ID",by.y="ID")
   }
-  
+
   trainDat$response <- extract (response,samplepoints)
   trainDat <- trainDat[complete.cases(trainDat),]
   set.seed(seed)
-  
-  
+
+
   set.seed(seed)
   if(design!="clustered"){
     model <- train(trainDat[,names(predictors)],
@@ -113,14 +154,12 @@ for (setting in 1:nrow(settings)){
                    importance=TRUE,
                    tuneGrid = expand.grid(mtry = c(2:length(names(predictors)))),
                    trControl = trainControl(method="cv",savePredictions = TRUE))
-    
-    
-    print("random cross-validation performance:")
-    print(model)
+
+
   }
   #if data are clustered, clustered CV is used:
   if(design=="clustered"){
-    folds <- CreateSpacetimeFolds(trainDat, spacevar="clstrID",k=nlusters)
+    folds <- CreateSpacetimeFolds(trainDat, spacevar="clstrID",k=nclusters)
     model <- train(trainDat[,names(predictors)],
                    trainDat$response,
                    method="rf",
@@ -129,9 +168,12 @@ for (setting in 1:nrow(settings)){
                    trControl = trainControl(method="cv",index=folds$index,savePredictions = TRUE))
 
   }
- # model <- train(trainDat[,names(predictors)],trainDat$response,
-#                 method="rf",importance=TRUE,tuneGrid = expand.grid(mtry = c(2:length(names(predictors)))),
- #                trControl = trainControl(method="cv"))
+
+
+
+  # model <- train(trainDat[,names(predictors)],trainDat$response,
+  #                 method="rf",importance=TRUE,tuneGrid = expand.grid(mtry = c(2:length(names(predictors)))),
+  #                trControl = trainControl(method="cv"))
 
   ## Prediction and error calculation
 
@@ -142,48 +184,104 @@ for (setting in 1:nrow(settings)){
   AOA <- aoa(predictors,model)
 
 
-  # Standard deviation from individual trees for comparison
-  # RFsd <- function(predictors,model){
-  #   prep <- as.data.frame(predictors)
-  #   prep[is.na(prep)] <- -9999
-  #   pred_all <- predict(model$finalModel,prep,predict.all=TRUE)
-  #   sds <-  apply(pred_all$individual,1,sd)
-  #   predsd <- predictors[[1]]
-  #   values(predsd)<-sds
-  #   values(predsd)[prep[,1]==-9999] <- NA
-  #   return(predsd)
-  # }
-#  predsd <- RFsd(predictors,model)
+  ### AOA calib
+  AOA_calib <- calibrate_aoa(AOA,model,window.size = 5,multiCV = TRUE,length.out = 10,calib="scam")
+  resultsTable$calibAOA_R2[setting] <- summary(lm(values(AOA_calib$expectedError)~values(truediff)))$r.squared
+  resultsTable$calibAOA_RMSE[setting] <- rmse(values(AOA_calib$expectedError),values(truediff))
 
+
+
+   #Standard deviation from individual trees for comparison
+   RFsd <- function(predictors,model){
+     prep <- as.data.frame(predictors)
+     prep[is.na(prep)] <- -9999
+     pred_all <- predict(model$finalModel,prep,predict.all=TRUE)
+     sds <-  apply(pred_all$individual,1,sd)
+     predsd <- predictors[[1]]
+     values(predsd)<-sds
+     values(predsd)[prep[,1]==-9999] <- NA
+     return(predsd)
+   }
+    predsd <- RFsd(predictors,model)
+ #   DI_calib <- calibrate_aoa(AOA,model,showplot=FALSE,maskAOA=FALSE)$expectedError
 
   ## Relationship with the true error
   resultsTable$DI_R2[setting] <- summary(lm(values(truediff)~values(AOA$DI)))$r.squared
- # resultsTable$RFSD_R2[setting] <- summary(lm(values(truediff)~values(predsd)))$r.squared
+   resultsTable$RFSD_R2[setting] <- summary(lm(values(truediff)~values(predsd)))$r.squared
   resultsTable$PredError_R2[setting] <- summary(lm(values(response)~values(prediction)))$r.squared
   resultsTable$PredError_RMSE[setting] <- rmse(values(response),values(prediction))
-  resultsTable$model_R2[setting] <- model$results$Rsquared[model$results$mtry==model$bestTune$mtry]
-  resultsTable$model_RMSE[setting] <- model$results$RMSE[model$results$mtry==model$bestTune$mtry]
+
+
+
+  preds <- model$pred[model$pred$mtry==model$bestTune$mtry,]
+
+  resultsTable$model_R2[setting] <- summary(lm(preds$pred~preds$obs))$r.squared
+  resultsTable$model_RMSE[setting] <- rmse(preds$pred,preds$obs)
+
+  resultsTable$model_R2_mcv[setting] <- model$results$Rsquared[model$results$mtry==model$bestTune$mtry]
+  resultsTable$model_RMSE_mcv[setting] <- model$results$RMSE[model$results$mtry==model$bestTune$mtry]
 
 
   ################################################################################
   # compare for different thresholds
   ################################################################################
-
-  for (th in 1:length(attributes(AOA)$aoa_stats$threshold_stats)){
-    thres <- attributes(AOA)$aoa_stats$threshold_stats[th]
-    thres_name <- names(attributes(AOA)$aoa_stats$threshold_stats)[th]
+  potentialthresholds <- c(attributes(AOA)$aoa_stats$threshold, attributes(AOA)$aoa_stats$threshold_stats)
+ # names(potentialthresholds)<- c("0%","default",names(attributes(AOA)$aoa_stats$threshold_stats),"2*max",
+  #                               "3*max","4*max")
+  names(potentialthresholds)<- c("default",names(attributes(AOA)$aoa_stats$threshold_stats))
+  for (th in 1:length(potentialthresholds)){
+    #thres <- attributes(AOA)$aoa_stats$threshold_stats[th]
+    thres <- potentialthresholds[th]
+    #thres_name <- names(attributes(AOA)$aoa_stats$threshold_stats)[th]
+    thres_name <- names(potentialthresholds)[th]
     predictionAOI <- prediction
     values(predictionAOI)[values(AOA$DI)>thres] <- NA
+
+    resultsTable[setting,paste0("ncell_NOTAOA_",thres_name)] <- sum(values(AOA$DI)>thres,na.rm=T)
+    resultsTable[setting,paste0("ncell_AOA_",thres_name)] <- sum(values(AOA$DI)<=thres,na.rm=T)
+
     resultsTable[setting,paste0("PredErrorAOA_R2_",thres_name)] <- summary(lm(values(response)~values(predictionAOI)))$r.squared
     resultsTable[setting,paste0("PredErrorAOA_RMSE_",thres_name)] <- rmse(values(response),values(predictionAOI))
-    predictionNOTAOI <- prediction
+
+
+    resultsTable[setting,paste0("RFSD_R2_AOA_",thres_name)] <- summary(lm(values(truediff)[values(AOA$AOA)==1]~values(predsd)[values(AOA$AOA)==1]))$r.squared
+    resultsTable[setting,paste0("RFSD_RMSE_AOA_",thres_name)] <- rmse(values(truediff)[values(AOA$AOA)==1],values(predsd)[values(AOA$AOA)==1])
+
+    resultsTable[setting,paste0("DI_R2_AOA_",thres_name)] <- summary(lm(values(truediff)[values(AOA$DI)<=thres]~values(AOA$DI)[values(AOA$DI)<=thres]))$r.squared
+    resultsTable[setting,paste0("DI_RMSE_AOA_",thres_name)] <- rmse(values(truediff)[values(AOA$DI)<=thres],values(AOA$DI)[values(AOA$DI)<=thres])
+
+
+   #     resultsTable[setting,paste0("RFSDDIcalib_R2_AOA_",thres_name)]    <- summary(lm(values(DI_calib)[values(AOA$AOA)==1]~values(predsd)[values(AOA$AOA)==1]))$r.squared
+  #      resultsTable[setting,paste0("RFSDDIcalib_RMSE_AOA_",thres_name)] <- rmse(values(predsd)[values(AOA$DI)<=thres],values(DI_calib)[values(AOA$DI)<=thres])
+
+        predictionNOTAOI <- prediction
     values(predictionNOTAOI)[values(AOA$DI)<=thres] <- NA
+
+
     if(sum(!is.na(values(predictionNOTAOI)))<2){
       resultsTable[setting,paste0("PredErrorNOTAOA_R2_",thres_name)] <- NA
       resultsTable[setting,paste0("PredErrorNOTAOA_RMSE_",thres_name)] <- NA
+      resultsTable[setting,paste0("RFSD_R2_NOTAOA_",thres_name)] <- NA
+      resultsTable[setting,paste0("RFSD_RMSE_NOTAOA_",thres_name)] <- NA
+      resultsTable[setting,paste0("DI_RMSE_NOTAOA_",thres_name)] <- NA
+      resultsTable[setting,paste0("DI_R2_NOTAOA_",thres_name)] <- NA
+    #  resultsTable[setting,paste0("RFSDDIcalib_R2_NOTAOA_",thres_name)] <- NA
+   #   resultsTable[setting,paste0("RFSDDIcalib_RMSE_NOTAOA_",thres_name)] <- NA
+
     }else{
       resultsTable[setting,paste0("PredErrorNOTAOA_R2_",thres_name)] <- summary(lm(values(response)~values(predictionNOTAOI)))$r.squared
       resultsTable[setting,paste0("PredErrorNOTAOA_RMSE_",thres_name)] <- rmse(values(response),values(predictionNOTAOI))
+      resultsTable[setting,paste0("RFSD_R2_NOTAOA_",thres_name)] <- summary(lm(values(truediff)[values(AOA$DI)>thres]~values(predsd)[values(AOA$DI)>thres]))$r.squared
+      resultsTable[setting,paste0("RFSD_RMSE_NOTAOA_",thres_name)] <- rmse(values(truediff)[values(AOA$DI)>thres],values(predsd)[values(AOA$DI)>thres])
+
+
+   resultsTable[setting,paste0("DI_R2_NOTAOA_",thres_name)] <- summary(lm(values(truediff)[values(AOA$DI)>thres]~values(AOA$DI)[values(AOA$DI)>thres]))$r.squared
+ resultsTable[setting,paste0("DI_RMSE_NOTAOA_",thres_name)] <- rmse(values(truediff)[values(AOA$DI)>thres],values(AOA$DI)[values(AOA$DI)>thres])
+
+   #    resultsTable[setting,paste0("RFSDDIcalib_R2_NOTAOA_",thres_name)]    <- summary(lm(values(DI_calib)[values(AOA$DI)>thres]~values(predsd)[values(AOA$DI)>thres]))$r.squared
+   #    resultsTable[setting,paste0("RFSDDIcalib_RMSE_NOTAOA_",thres_name)] <- rmse(values(predsd)[values(AOA$DI)>thres],values(DI_calib)[values(AOA$DI)>thres])
+
+
     }
   }
 
--- src/CaseStudyScenarios_figures.R
+++ src/CaseStudyScenarios_figures.R
@@ -48,23 +48,12 @@ performanceNOTAOA <- c()
 potentialthres <-unique(substr(names(dat)[grep("PredErrorAOA_R2",names(dat))],17,24))
 potentialthres <- potentialthres[1:9]
 
-#for (thres in potentialthres){
-#performanceAOA <- rbind(performanceAOA,data.frame("thres"=thres,
-#                                                  "ME"=me(dat$model_RMSE,dat[,paste0("PredErrorAOA_RMSE_",thres)])))
-#performanceNOTAOA <- rbind(performanceNOTAOA,data.frame("thres"=thres,
-#                                                        "ME"=me(dat$model_RMSE,dat[,paste0("PredErrorNOTAOA_RMSE_",thres)])))
-#}
 
-# ideal threshold:
-#bestthres <- performanceAOA$thres[which(abs(performanceAOA$ME)==min(abs(0-performanceAOA$ME)))]
-#print(bestthres)
-bestthres <- "default"
+thres <- "default"
 
 #### How well does the model CV error represents the prediction error inside and outside the AOA?
 
-for (thres in potentialthres){
-pdfname<-sub("%","",paste0("../figures/comparison_modelRuns_scatter_",thres,".pdf"))
-pdf(pdfname,width=9,height=5)
+pdf("../figures/comparison_modelRuns_scatter.pdf",width=9,height=5)
 par(mfrow=c(1,2))
 
 lim <- c(min(c(dat$model_RMSE_mcv,dat[,paste0("PredErrorAOA_RMSE_",thres)],dat$model_RMSE_mcv,dat[,paste0("PredErrorNOTAOA_RMSE_",thres)]),na.rm=T),
@@ -75,8 +64,6 @@ plot(dat$model_RMSE_mcv~dat[,paste0("PredErrorAOA_RMSE_",thres)],
 points(caseStudy$model_RMSE_mcv~caseStudy[,paste0("PredErrorAOA_RMSE_",thres)],col="red",pch=16)
 legend("topleft",legend="a",bty="n")
 abline(0,1)
-#lim <- c(min(c(dat$model_RMSE,dat[,paste0("PredErrorNOTAOA_RMSE_",thres)]),na.rm=T),
-#         max(c(dat$model_RMSE,dat[,paste0("PredErrorNOTAOA_RMSE_",thres)]),na.rm=T))
 plot(dat$model_RMSE_mcv~dat[,paste0("PredErrorNOTAOA_RMSE_",thres)],
      xlim=lim,ylim=lim,
      xlab="RMSE prediction (outside AOA)",ylab="RMSE model CV")
@@ -84,37 +71,3 @@ points(caseStudy$model_RMSE_mcv~caseStudy[,paste0("PredErrorNOTAOA_RMSE_",thres)
 legend("topleft",legend="b",bty="n")
 abline(0,1)
 dev.off()
-}
-
-####################
-
-plot(resultsTable$RFSD_RMSE_AOA_default, resultsTable$DIcalib_RMSE_AOA_default,xlim=c(0,0.6),ylim=c(0,0.6))
-abline(0,1)
-#abline(lm(resultsTable$DIcalib_RMSE_AOA_default~resultsTable$RFSD_RMSE_AOA_default),col="red")
-
-plot(resultsTable$RFSD_RMSE_NOTAOA_default, resultsTable$DIcalib_RMSE_NOTAOA_default,xlim=c(0,0.6),ylim=c(0,0.6))
-
-#dat_bx <- data.frame("R2"=c(resultsTable$RFSD_R2,resultsTable$DI_R2,
-#                                resultsTable$RFSD_R2_AOA_default, resultsTable$DIcalib_R2_AOA_default,
-#                                resultsTable$RFSD_R2_NOTAOA_default, resultsTable$DIcalib_R2_NOTAOA_default),
-#                     "Type"=c(rep("SD",nrow(dat)),rep("DI",nrow(dat))),
-#                                "Loc"=c(rep("All",nrow(dat)*2),rep("AOA",nrow(dat)*2),rep("Outside",nrow(dat)*2))) 
-
-
-boxplot(dat$PredError_R2,dat$RFSD_R2,dat$DI_R2,
-        dat$RFSD_R2_AOA_default, dat$DIcalib_R2_AOA_default,
-        dat$RFSD_R2_NOTAOA_default, dat$DIcalib_R2_NOTAOA_default, notch=T,
-        col=c("darkgrey","lightgrey"),xaxt='n',outline = F)
-axis(side=1, c(1.5,3.5,5.5), labels=c("all","AOA","outside") )
-points(colMeans(dat[,c("RFSD_R2","DI_R2","RFSD_R2_AOA_default","DIcalib_R2_AOA_default","RFSD_R2_NOTAOA_default",
-                                "DIcalib_R2_NOTAOA_default")],na.rm=T),pch=16)
-legend("topleft",fill=c("darkgrey","lightgrey"),legend=c("Ensemble","DI"),bty="n")
-
-
-###HIER NOCH INSGESAMT!!!
-boxplot(dat$RFSD_RMSE_AOA_default, dat$DIcalib_RMSE_AOA_default,
-        dat$RFSD_RMSE_NOTAOA_default, dat$DIcalib_RMSE_NOTAOA_default, notch=T,
-        col=c("darkgrey","lightgrey"),xaxt='n',outline = F)
-legend("topleft",fill=c("darkgrey","lightgrey"),legend=c("Ensemble","DI"),bty="n")
-
-
